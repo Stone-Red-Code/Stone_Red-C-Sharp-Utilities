e7e36073b253ceceedeb35fa6812a556b8f48df8
Stone-Red-Code <56473591+Stone-Red-Code@users.noreply.github.com>
2021-11-12 17:29:11 +0100

    - Fix `.gitignore` - Fix some spelling mistakes

--- Stone_Red-C-Sharp-Utilities/HttpClient.cs
+++ Stone_Red-C-Sharp-Utilities/HttpClient.cs
@@ -5,13 +5,30 @@ using System.Threading.Tasks;
 
 namespace Stone_Red_Utilities.Http
 {
+    /// <summary>
+    /// Extensions for <see cref="HttpClient"/>
+    /// </summary>
     public static class HttpClientExtentions
     {
+        /// <summary>
+        /// Reads the JSON returned from the website and converts it into an object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="httpClient"></param>
+        /// <param name="requestUri"></param>
+        /// <returns></returns>
         public static async Task<T> GetJsonObjectAsync<T>(this HttpClient httpClient, string requestUri)
         {
             return JsonSerializer.Deserialize<T>(await httpClient.GetStringAsync(requestUri));
         }
 
+        /// <summary>
+        /// Reads the JSON returned from the website and converts it into an object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="httpClient"></param>
+        /// <param name="requestUri"></param>
+        /// <returns></returns>
         public static async Task<T> GetJsonObjectAsync<T>(this HttpClient httpClient, Uri requestUri)
         {
             return JsonSerializer.Deserialize<T>(await httpClient.GetStringAsync(requestUri));
